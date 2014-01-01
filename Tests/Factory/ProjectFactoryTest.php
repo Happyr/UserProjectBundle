b19ff6202ec6f6aadaaf10abf135e53e5b07be01
Nyholm <tobias.nyholm@gmail.com>
2014-01-01 13:40:21 +0100

    added travis

---
+++ Tests/Factory/ProjectFactoryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace HappyR\UserProjectBundle\Tests\Factory;
+
+
+/**
+ * Class ProjectFactoryTest
+ *
+ * @author Tobias Nyholm
+ *
+ */
+class ProjectFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+}
+ 
