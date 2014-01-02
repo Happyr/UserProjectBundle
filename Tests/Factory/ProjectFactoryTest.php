ce911ad5a7f7c0bc1fbec347465e796908bae28d
Nyholm <tobias.nyholm@gmail.com>
2014-01-02 11:34:40 +0100

    minor

--- Tests/Factory/ProjectFactoryTest.php
+++ Tests/Factory/ProjectFactoryTest.php
@@ -22,13 +22,12 @@ class ProjectFactoryTest extends \PHPUnit_Framework_TestCase
         $project=m::mock('HappyR\UserProjectBundle\Entity\Project');
         $project
             ->shouldReceive('setName')->with('_private_4711')->andReturn($project)
-            ->shouldReceive('setPublic')->with(false)->andReturn($project);
+            ->shouldReceive('setPublic')->with(false)->andReturn($project)
+            ->shouldReceive('addUser')->with($user)->andReturn($project);
 
         $factory=new ProjectFactory(
             m::mock('Doctrine\Common\Persistence\ObjectManager'),
             m::mock('HappyR\UserProjectBundle\Manager\PermissionManager')
-                ->shouldReceive('addUser')->with($project, $user, 'MASTER')
-                ->getMock()
         );
 
         $factory->makePrivate($project, $user);
